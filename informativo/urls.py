a4fd9b47a6dd91120571f60240dd07c52173a1c0
raquelbrandino <raquelbrandino@hotmail.com>
2019-08-09 22:10:00 -0300

    inserir informações sobre as ferramentas

--- informativo/urls.py
+++ informativo/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from paginas.views import home
+from paginas.views import home, ferramentas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('ferramentas', ferramentas)
 ]
